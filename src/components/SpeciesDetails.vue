7d62321c017ac7587e8f724b021ecdfa34c9ff1f
mateuszmu <unnamed109@wp.pl>
2025-10-02 16:21:44 +0200

    zmiana tabeli na siatke BS- pierwszy zmieniony plik- SpeciesDetails.vue

--- src/components/SpeciesDetails.vue
+++ src/components/SpeciesDetails.vue
@@ -6,69 +6,93 @@
     <error-component :error="error"/>
   </div>
   <div v-else-if="species" class="main">
-    <h2>Species data</h2>
-    <div>
-      <table>
-        <tr>
-          <td>Species:</td>
-          <td>{{ species.name }}</td>
-        </tr>
-        <tr>
-          <td>Classification:</td>
-          <td>
-            {{ capitalize(species.classification) }}
-          </td>
-        </tr>
-        <tr>
-          <td>Designation:</td>
-          <td>
-            {{ capitalize(species.designation) }}
-          </td>
-        </tr>
-        <tr>
-          <td>Average height:</td>
-          <td>{{ capitalize(species.average_height) }}</td>
-        </tr>
-        <tr>
-          <td>Skin colors:</td>
-          <td>
-            {{ capitalize(species.skin_colors) }}
-          </td>
-        </tr>
-        <tr>
-          <td>Hair colors:</td>
-          <td>
-            {{ capitalize(species.hair_colors) }}
-          </td>
-        </tr>
-        <tr>
-          <td>Eye colors:</td>
-          <td>
-            {{ capitalize(species.eye_colors) }}
-          </td>
-        </tr>
-        <tr>
-          <td>Average lifespan:</td>
-          <td>{{ species.average_lifespan }}</td>
-        </tr>
-      </table>
+    <h2 >Species data</h2>
+    <div >
 
-      <router-link to="/species" class="btn text-decoration-none">
-        Back to the list
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-arrow-return-left"
-            viewBox="0 0 16 16"
-        >
-          <path
-              fill-rule="evenodd"
-              d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"
-          />
-        </svg>
-      </router-link>
+      <div class="container text-center">
+        <div class="row justify-content-center    ">
+          <div class="col-2">
+            Species:
+          </div>
+          <div class="col-2">
+            {{ species.name }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Classification:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.classification) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Designation:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.designation) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Average height:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.average_height) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Skin colors:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.skin_colors) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Hair colors:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.hair_colors) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Eye colors:
+          </div>
+          <div class="col-2">
+            {{ capitalize(species.eye_colors) }}
+          </div>
+        </div>
+        <div class="row justify-content-center ">
+          <div class="col-2">
+            Average lifespan:
+          </div>
+          <div class="col-2">
+            {{ species.average_lifespan }}
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid text-center">
+        <router-link to="/species" class="btn text-decoration-none">
+          Back to the list
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-return-left"
+              viewBox="0 0 16 16"
+          >
+            <path
+                fill-rule="evenodd"
+                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"
+            />
+          </svg>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
